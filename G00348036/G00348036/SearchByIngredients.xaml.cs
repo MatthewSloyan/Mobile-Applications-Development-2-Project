870a5d303e2619c007ef1ef6fed5cece6cf44e57
Matthew Sloyan <g00348036@gmit.ie>
2019-02-17 17:40:05 +0000

    I added the ability to add multiple entry boxes, so the user can enter multiple ingredients. Also I implemented a scroll view for this.

--- G00348036/G00348036/SearchByIngredients.xaml.cs
+++ G00348036/G00348036/SearchByIngredients.xaml.cs
@@ -24,6 +24,28 @@ namespace G00348036
         
         private void BtnAdd_Clicked(object sender, EventArgs e)
         {
+            var layout = new StackLayout();
+            layout.Orientation = StackOrientation.Horizontal; 
+
+            var entIngredient = new Entry
+            {
+                Placeholder = "Enter Your Ingredient",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var button = new Button
+            {
+                Text = "Add",
+                WidthRequest = 50
+            };
+
+            layout.Children.Add(entIngredient);
+            layout.Children.Add(button);
+            slIngredients.Children.Add(layout);
+        }
+
+        private void BtnSearchByIngredient_Clicked(object sender, EventArgs e)
+        {
 
         }
     }
