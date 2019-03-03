096df126cdd8a87637908a705d1a448990e46843
Matthew Sloyan <g00348036@gmit.ie>
2019-03-03 00:36:29 +0000

    Added initial API functionality for returning recipes based on input ingredients.

--- G00348036/G00348036/SearchByIngredients.xaml.cs
+++ G00348036/G00348036/SearchByIngredients.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using unirest_net.http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +13,23 @@ namespace G00348036
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SearchByIngredients : ContentPage
 	{
-		public SearchByIngredients ()
+        //Global list
+        ArrayList list;
+
+        public SearchByIngredients ()
 		{
 			InitializeComponent ();
+            setUpComponents();
 		}
+
+        private void setUpComponents()
+        {
+            //set up initial two input boxes
+            //createAddIngredientInput();
+           // createAddIngredientInput();
+
+            list = new ArrayList();
+        }
 
         private void EntIngredient_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -24,8 +38,15 @@ namespace G00348036
         
         private void BtnAdd_Clicked(object sender, EventArgs e)
         {
+            createAddIngredientInput();
+        }
+
+        private void createAddIngredientInput()
+        {
             var layout = new StackLayout();
-            layout.Orientation = StackOrientation.Horizontal; 
+            layout.Orientation = StackOrientation.Horizontal;
+            //layout.StyleId = "slAddIngredient" + 1;
+            list.Add("slAddIngredient");
 
             var entIngredient = new Entry
             {
@@ -33,20 +54,33 @@ namespace G00348036
                 HorizontalOptions = LayoutOptions.FillAndExpand
             };
 
-            var button = new Button
+            var addButton = new Button
             {
                 Text = "Add",
-                WidthRequest = 50
+                WidthRequest = 60
             };
 
+            var deleteButton = new Button
+            {
+                Text = "Del",
+                WidthRequest = 60
+            };
+
+            addButton.Clicked += BtnAdd_Clicked;
             layout.Children.Add(entIngredient);
-            layout.Children.Add(button);
+            layout.Children.Add(addButton);
+            layout.Children.Add(deleteButton);
             slIngredients.Children.Add(layout);
         }
 
         private void BtnSearchByIngredient_Clicked(object sender, EventArgs e)
         {
+            string s = entIngredient1.Text;
 
+            Task<HttpResponse<SearchByIngredientsData>> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ranking=1&fillIngredients=true&ingredients=apples%2Cflour%2Csugar")
+            .header("X-RapidAPI-Key", "583ced2f01mshf4b63cc4f7b49f7p130fc5jsn4e92edadc525")
+            .asJsonAsync<SearchByIngredientsData>();
+            //.asJson();
         }
     }
 }
