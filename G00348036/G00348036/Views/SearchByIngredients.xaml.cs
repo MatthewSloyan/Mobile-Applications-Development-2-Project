f07a38e4cb4dd17f8917bc8820effddcf87344fc
Matthew Sloyan <g00348036@gmit.ie>
2019-03-23 19:25:20 +0000

    When search it selected on SearchByIngredients page an arraylist of the input items is gathered.

--- G00348036/G00348036/Views/SearchByIngredients.xaml.cs
+++ G00348036/G00348036/Views/SearchByIngredients.xaml.cs
@@ -10,6 +10,7 @@ namespace G00348036
 	{
         //Global list
         ArrayList list;
+        int count = 0;
 
         public SearchByIngredients ()
 		{
@@ -21,11 +22,8 @@ namespace G00348036
 
         private void setUpComponents()
         {
-            //set up initial two input boxes
-            //createAddIngredientInput();
-           // createAddIngredientInput();
-
             list = new ArrayList();
+            createAddIngredientInput();
         }
 
         private void EntIngredient_TextChanged(object sender, TextChangedEventArgs e)
@@ -39,20 +37,21 @@ namespace G00348036
 
             //var layout = new StackLayout();
             //layout = (BindingContext as SearchByIngredientsViewModel).createAddIngredientInput();
-            //slIngredients.Children.Add(layout);
+            //slIngredients.Children.Add(layout); 
         }
 
         private void createAddIngredientInput()
         {
             var layout = new StackLayout();
             layout.Orientation = StackOrientation.Horizontal;
-            //layout.StyleId = "slAddIngredient" + 1;
-            list.Add("slAddIngredient");
+            layout.StyleId = "slAddIngredient";
+            list.Add(count++);
 
             var entIngredient = new Entry
             {
                 Placeholder = "Enter Your Ingredient",
-                HorizontalOptions = LayoutOptions.FillAndExpand
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                StyleId = "entryIngredient"
             };
 
             var addButton = new Button
@@ -61,21 +60,60 @@ namespace G00348036
                 WidthRequest = 70
             };
 
-            var deleteButton = new Button
-            {
-                Text = "Del",
-                WidthRequest = 70
-            };
+            //var deleteButton = new Button
+            //{
+            //    Text = "Del",
+            //    WidthRequest = 70
+            //};
 
             addButton.Clicked += BtnAdd_Clicked;
+            //deleteButton.Clicked += BtnDelete1_Clicked;
             layout.Children.Add(entIngredient);
             layout.Children.Add(addButton);
-            layout.Children.Add(deleteButton);
+            //layout.Children.Add(deleteButton);
             slIngredients.Children.Add(layout);
         }
 
+        //private void BtnDelete1_Clicked(object sender, EventArgs e)
+        //{
+        //    for (int i = 0; i < list.Count; ++i)
+        //    {
+        //        if (list[i].ToString() == (sender as Button).StyleId)
+        //        {
+        //            slIngredients.Children.RemoveAt(i);
+        //            Console.WriteLine(list[i]);
+        //        }
+               
+        //    }
+
+        //    //if (slIngredients.Children.Count != 1)
+        //    //{
+        //    //    slIngredients.Children.RemoveAt(1);
+        //    //}
+        //}
+
         private void BtnSearchByIngredient_Clicked(object sender, EventArgs e)
         {
+            ArrayList ingredientsList = new ArrayList();
+
+            foreach (View item in slIngredients.Children)
+            {
+                if (item.StyleId == "slAddIngredient")
+                {
+                    var sl = (StackLayout)item;
+
+                    foreach (View slItems in sl.Children)
+                    {
+                        if (slItems.StyleId == "entryIngredient")
+                        {
+                            var entry = (Entry)slItems;
+                            ingredientsList.Add(entry.Text);
+                        }
+                    }
+                }
+            }
+
+            //slIngredients.Children.
             string URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ranking=1&fillIngredients=true&ingredients=apples%2Cflour%2Csugar";
 
             //List<SearchByIngredientsData> results = GetApiData<SearchByIngredientsData>(URL);
