339c6baec47062424c3e140c8227ddc3a56deec4
Matthew Sloyan <g00348036@gmit.ie>
2019-03-29 22:10:39 +0000

    I have added a HTTP post request that posts json data to Google Vision and returns a response based on the uploaded image. It returns annotations for the image which will be populated as ingredients.

--- G00348036/G00348036/Views/SearchByImage.xaml.cs
+++ G00348036/G00348036/Views/SearchByImage.xaml.cs
@@ -1,20 +1,15 @@
-﻿using Google.Apis.Auth.OAuth2;
-using Google.Cloud.Vision.V1;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Plugin.Media;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
-
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace G00348036.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SearchByImage : ContentPage
 	{
 		public SearchByImage ()
@@ -24,47 +19,6 @@ namespace G00348036.Views
 
         private void BtnTakePicture_Clicked(object sender, EventArgs e)
         {
-            //ImageJsonData.Image i = new ImageJsonData.Image();
-            //i.content = "image";
-
-            //ImageJsonData.Feature f = new ImageJsonData.Feature
-            //{
-            //    type = "FACE_DETECTION",
-            //    maxResults = 10
-            //};
-
-            ////List<ImageJsonData.Feature> features = new List<ImageJsonData.Feature>();
-            ////features.Add(f);
-
-            //ImageJsonData.Request r = new ImageJsonData.Request();
-            //r.image = i;
-            //r.features = new List<ImageJsonData.Feature>
-            //{
-            //    type = "FACE_DETECTION",
-            //    maxResults = 10
-            //};
-
-            //List<ImageJsonData.Request> requests = new List<ImageJsonData.Request>();
-            //requests.Add(r);
-
-            //ImageJsonData.RootObject full = new ImageJsonData.RootObject();
-            //full.requests.Add(r);
-
-            var obj = new
-            {
-                requests = new[] {
-                    new  {
-                         image = new { content  = "image" },
-                         features = new[] {
-                             new  { type = "LABEL_DETECTION", maxResults = 10}
-                         }
-                    }
-                }
-            };
-
-            JsonConvert.SerializeObject(obj);
-            System.Diagnostics.Debug.WriteLine(JsonConvert.SerializeObject(obj).ToString());
-
             TakePictureAsync();
         }
 
@@ -81,7 +35,7 @@ namespace G00348036.Views
             var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
             {
                 Directory = "Sample",
-                Name = "test.jpg"
+                Name = "image.jpg"
             });
 
             if (file == null)
@@ -92,34 +46,54 @@ namespace G00348036.Views
             PhotoImage.Source = Xamarin.Forms.ImageSource.FromStream(() =>
             {
                 var stream = file.GetStream();
-                setUpAPI(file.Path);
+
+                // Set up the API call
+                SetUpAPI(file.Path);
                 return stream;
             });
-
-            
         }
 
-        private void setUpAPI(string path)
+        private void SetUpAPI(string path)
         {
-            //var credential = GoogleCredential.FromFile("C:/Users/Matthew/Source/Repos/SearchByIngredientsApp-333bfa73dcc2.json");
-            // Instantiates a client
-            //var client = ImageAnnotatorClient.Create();
-            //// Load the image file into memory
-            //var image = Google.Cloud.Vision.V1.Image.FromFile(path);
-            //// Performs label detection on the image file
-            //var response = client.DetectLabels(image);
-            //foreach (var annotation in response)
-            //{
-            //    if (annotation.Description != null)
-            //    {
-            //        Console.WriteLine(annotation.Description);
-            //        System.Diagnostics.Debug.WriteLine(annotation.Description);
-            //    }
-            //}
-            //var credential = GoogleCredential.GetApplicationDefault();
-            //var credential = GoogleCredential.FromFile("C:/Users/Matthew/Source/Repos/SearchByIngredientsApp-333bfa73dcc2.json");
+            // Convert the saved image to base64 format to send via http
+            byte[] bytes = File.ReadAllBytes(path);
+            string file = Convert.ToBase64String(bytes);
 
+            // Based on the JSON format and layout that google Vision requires build Json data using information.
+            var obj = new
+            {
+                requests = new[] {
+                    new  {
+                         image = new { content  = file },
+                         features = new[] {
+                             new  { type = "LABEL_DETECTION", maxResults = 10}
+                         }
+                    }
+                }
+            };
 
+            // To implement the HTTP request I found suitable code below but modified it and improved for my own needs.
+            // https://stackoverflow.com/questions/9145667/how-to-post-json-to-a-server-using-c
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAz2XlLIlDE4NHoCENCrliqW1Motsk8WHY");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                // Serialize the new object created above into JSON format for sending via HTTP
+                streamWriter.Write(JsonConvert.SerializeObject(obj));
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            // Recieve response from Google Vision API call
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                System.Diagnostics.Debug.WriteLine(result);
+            }
         }
     }
 }
