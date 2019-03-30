4d6cd1e58e3a7bca87e1c4e9ffbc237661714428
Matthew Sloyan <g00348036@gmit.ie>
2019-03-30 15:00:53 +0000

    Set up Favourites page with basic XAML and binding context.

---
+++ G00348036/G00348036/ModelViews/FavouritesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G00348036
+{
+    class FavouritesViewModel
+    {
+
+    }
+}
---
+++ G00348036/G00348036/Views/Favourites.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace G00348036.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Favourites : ContentPage
+	{
+		public Favourites ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new FavouritesViewModel();
+        }
+
+        private void LvFavourites_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteFromFavourites_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
