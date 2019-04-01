cc4747323ac291d656e65efe0926e0a106a17791
Matthew Sloyan <g00348036@gmit.ie>
2019-04-01 22:04:07 +0100

    Implemented full the display of favourite repices on homepage in a horizontal scroll view.

--- G00348036/G00348036/Views/MainPage.xaml.cs
+++ G00348036/G00348036/Views/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using G00348036.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace G00348036
 {
     public partial class MainPage : ContentPage
     {
+        //global list of favourite recipes
+        private ObservableCollection<FavouriteRecipesData> Results { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
@@ -18,51 +22,62 @@ namespace G00348036
         
         private void SetUpComponents()
         {
-            var layout = new StackLayout
-            {
-                WidthRequest = 100,
-                Orientation = StackOrientation.Vertical,
-            };
-
-            var image = new Image
-            {
-                Source = "dfdf",
-                HeightRequest = 100,
-                WidthRequest = 100,
-                HorizontalOptions = LayoutOptions.CenterAndExpand
-            };
-
-            var title = new Label
-            {
-                Text = "Add",
-                FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.CenterAndExpand
-            };
-
-            var id = new Label
-            {
-                Text = "Add",
-                IsVisible = false
-            };
+            Results = Utils.getListFromFile<FavouriteRecipesData>();
             
-            //create a tap gesture object and attach to stacklayout - collection of tap gestures associated with image
-            TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
-            tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
-            tapGestureRecognizer.NumberOfTapsRequired = 1;
+            if (Results == null)
+                return;
 
-            //add to image
-            layout.GestureRecognizers.Add(tapGestureRecognizer);
-            
-            layout.Children.Add(image);
-            layout.Children.Add(title);
-            layout.Children.Add(id);
-            slFavourites.Children.Add(layout);
+            foreach (var item in Results)
+            {
+                var layout = new StackLayout
+                {
+                    WidthRequest = 100,
+                    Orientation = StackOrientation.Vertical,
+                    StyleId = item.id
+                };
+
+                var image = new Image
+                {
+                    Source = item.image,
+                    HeightRequest = 100,
+                    WidthRequest = 100,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                };
+
+                var title = new Label
+                {
+                    Text = item.title,
+                    FontAttributes = FontAttributes.Bold,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                };
+
+                var id = new Label
+                {
+                    Text = item.id,
+                    IsVisible = false
+                };
+
+                //create a tap gesture object and attach to stacklayout
+                TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+                tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+                tapGestureRecognizer.NumberOfTapsRequired = 1;
+
+                //add to image
+                layout.GestureRecognizers.Add(tapGestureRecognizer);
+
+                layout.Children.Add(image);
+                layout.Children.Add(title);
+                layout.Children.Add(id);
+                slFavourites.Children.Add(layout);
+            }
         }
 
         #region Event handlers
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
-           
+            StackLayout slSender = (StackLayout)sender;
+
+            Navigation.PushAsync(new RecipeInformation(slSender.StyleId));
         }
         #endregion
         
