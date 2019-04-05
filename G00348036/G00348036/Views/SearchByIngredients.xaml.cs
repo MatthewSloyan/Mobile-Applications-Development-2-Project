f6a073cbf01b2653e0a5a552427d0e4a2abae68e
Matthew Sloyan <g00348036@gmit.ie>
2019-04-05 21:44:25 +0100

    Implemented issue #3, however it is not ideal as it just removes the last child but it solves the problem of accidentally adding too many entries. I tried to remove the child at the index but I couldn't figure it out, so I will possibly try it again if I have time.

--- G00348036/G00348036/Views/SearchByIngredients.xaml.cs
+++ G00348036/G00348036/Views/SearchByIngredients.xaml.cs
@@ -9,7 +9,8 @@ namespace G00348036
 	public partial class SearchByIngredients : ContentPage
 	{
         //Global list
-        ArrayList list;
+        //ArrayList list = new ArrayList();
+       // int count = 0;
 
         public SearchByIngredients ()
 		{
@@ -40,6 +41,7 @@ namespace G00348036
             var layout = new StackLayout();
             layout.Orientation = StackOrientation.Horizontal;
             layout.StyleId = "slAddIngredient";
+            //list.Add(count++);
 
             var entIngredient = new Entry
             {
@@ -50,42 +52,73 @@ namespace G00348036
 
             var addButton = new Button
             {
-                Text = "Add",
+                Text = "ADD",
                 WidthRequest = 70,
                 Style = (Style)Application.Current.Resources["ButtonStyleFull"]
             };
 
-            //var deleteButton = new Button
-            //{
-            //    Text = "Del",
-            //    WidthRequest = 70
-            //};
+            var deleteButton = new Button
+            {
+                Text = "DEL",
+                WidthRequest = 70,
+                Style = (Style)Application.Current.Resources["ButtonStyleFull"],
+                StyleId = slIngredients.Children.Count.ToString()
+            };
 
             addButton.Clicked += BtnAdd_Clicked;
-            //deleteButton.Clicked += BtnDelete1_Clicked;
+            deleteButton.Clicked += BtnDelete_Clicked;
             layout.Children.Add(entIngredient);
             layout.Children.Add(addButton);
-            //layout.Children.Add(deleteButton);
+            layout.Children.Add(deleteButton);
             slIngredients.Children.Insert(slIngredients.Children.Count - 1, layout);
         }
 
-        //private void BtnDelete1_Clicked(object sender, EventArgs e)
-        //{
-        //    for (int i = 0; i < list.Count; ++i)
-        //    {
-        //        if (list[i].ToString() == (sender as Button).StyleId)
-        //        {
-        //            slIngredients.Children.RemoveAt(i);
-        //            Console.WriteLine(list[i]);
-        //        }
-               
-        //    }
+        private void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            // Remove the last child from the stacklayout, which removes the entry and two buttons.
+            // I tried to remove the exact index child to no avail.. Tries can be seen in previous commits
+            // It's not the most ideal solution but it solves the problem of accidentally adding too many entries 
+            if(slIngredients.Children.Count != 1) 
+                slIngredients.Children.RemoveAt(slIngredients.Children.Count-1);
 
-        //    //if (slIngredients.Children.Count != 1)
-        //    //{
-        //    //    slIngredients.Children.RemoveAt(1);
-        //    //}
-        //}
+            //for (int i = 1; i <= slIngredients.Children.Count-1; i++)
+            //{
+            //    if (i.ToString() == (sender as Button).StyleId)
+            //    {
+            //        slIngredients.Children.RemoveAt(i);
+            //        //count--;
+            //        //System.Diagnostics.Debug.WriteLine(count);
+            //    }
+            //}
+            //foreach (View item in slIngredients.Children)
+            //{
+            //    if (item.StyleId == "slAddIngredient")
+            //    {
+            //        var sl = (StackLayout)item;
+                    
+            //        foreach (View slItems in sl.Children)
+            //        {
+            //            //var sl = (StackLayout)item 
+            //            if (i.ToString() == (sender as Button).StyleId)
+            //            {
+            //                slIngredients.Children.RemoveAt(i);
+            //                count--;
+            //                System.Diagnostics.Debug.WriteLine(count);
+
+            //                foreach (View item in slIngredients.Children)
+            //                {
+
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            //if (slIngredients.Children.Count != 1)
+            //{
+            //    slIngredients.Children.RemoveAt(1);
+            //}
+        }
 
         private void BtnSearchByIngredient_Clicked(object sender, EventArgs e)
         {
@@ -111,8 +144,11 @@ namespace G00348036
                     } 
                 }
             }
-            
-            string URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=15&ranking=1&fillIngredients=true&ingredients=" + dynamicString;
+
+            // Strip out any white space just incase
+            string strippedString = dynamicString.Replace(" ", string.Empty);
+
+            string URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=15&ranking=1&fillIngredients=true&ingredients=" + strippedString;
 
             Navigation.PushAsync(new Recipes(URL, 1));
         }
