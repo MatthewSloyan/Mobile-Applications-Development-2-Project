8fa3250247d3c1a2106e1365abe6d744f6ef89e2
Matthew Sloyan <g00348036@gmit.ie>
2019-04-07 00:08:53 +0100

    Fixed issue #5, where instructions were not displayed. It was due to the binding of the overall stackpanel which I have now separated.

--- G00348036/G00348036/ModelViews/RecipeInformationViewModel.cs
+++ G00348036/G00348036/ModelViews/RecipeInformationViewModel.cs
@@ -11,7 +11,7 @@ namespace G00348036
 
         //global list of recipes
         public RecipeInformationData Result { get; set; }
-        public ObservableCollection<InstructionStepsData> Instructions { get; set; }
+        public List<ExtendedIngredient> ExtendedIngredients { get; set; }
         public List<Step> Steps { get; set; }
 
         // Contructor
@@ -25,12 +25,12 @@ namespace G00348036
         {
             try
             {
-                string url;
-                url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + Id + "/information";
+                string url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + Id + "/information";
                 Result = Utils.GetSingleApiData<RecipeInformationData>(url);
+                ExtendedIngredients = Result.extendedIngredients;
 
-                url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + Id + "/analyzedInstructions?stepBreakdown=true";
-                Instructions = Utils.GetApiData<InstructionStepsData>(url);
+                string urlIntructions = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + Id + "/analyzedInstructions?stepBreakdown=true";
+                ObservableCollection<InstructionStepsData> Instructions = Utils.GetApiData<InstructionStepsData>(urlIntructions);
                 Steps = Instructions[0].steps;
             }
             catch (Exception)
--- G00348036/G00348036/Models/InstructionStepsData.cs
+++ G00348036/G00348036/Models/InstructionStepsData.cs
@@ -12,19 +12,7 @@ namespace G00348036
 
     public class Step
     {
-        public int number { get; set; }
+        public string number { get; set; }
         public string step { get; set; }
-        public List<object> ingredients { get; set; }
-        public List<object> equipment { get; set; }
-        public Length length { get; set; }
     }
-
-    public class Length
-    {
-        public int number { get; set; }
-        public string unit { get; set; }
-    }
-
-    
-
 }
