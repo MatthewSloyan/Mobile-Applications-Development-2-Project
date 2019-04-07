a3c2aabc129ef22d67d50a02d6d5593b7f3fee28
Matthew Sloyan <g00348036@gmit.ie>
2019-04-07 11:05:38 +0100

    Improved recipe information page, however white space after list views still persists. Will document in issue #5

--- G00348036/G00348036/ModelViews/RecipeInformationViewModel.cs
+++ G00348036/G00348036/ModelViews/RecipeInformationViewModel.cs
@@ -29,6 +29,12 @@ namespace G00348036
                 Result = Utils.GetSingleApiData<RecipeInformationData>(url);
                 ExtendedIngredients = Result.extendedIngredients;
 
+                // As image url doesn't include the base URL prepend onto image for displaying.
+                for (int i = 0; i < ExtendedIngredients.Count; ++i)
+                {
+                    ExtendedIngredients[i].image = "https://spoonacular.com/cdn/ingredients_100x100/" + ExtendedIngredients[i].image;
+                }
+
                 string urlIntructions = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + Id + "/analyzedInstructions?stepBreakdown=true";
                 ObservableCollection<InstructionStepsData> Instructions = Utils.GetApiData<InstructionStepsData>(urlIntructions);
                 Steps = Instructions[0].steps;
